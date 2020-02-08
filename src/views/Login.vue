89ed3ac8104b717ff4d2c9dd63a0f2234e1c7f33
anabahena <ana-bahena@live.com.mx>
2020-02-08 15:25:57 -0600

    second commit

---
+++ src/views/Login.vue
@@ -0,0 +1,27 @@
+<template>
+    
+    <div class="login">
+        <h3>Sign In</h3>
+        <input type="text" placeholder="Email">
+        <input type="text" placeholder="Password">
+        <button>Connection</button>
+        <p>You don´t have an account? Yu can create one</p>
+
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'login',
+    data(){
+        return{}
+    },
+    methods:{
+    }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
